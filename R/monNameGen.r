fe32a28d19f1a67401a3f8c686a6d870dba31505
Derek Nedveck <nedveck@gmail.com>
2019-11-29 00:29:40 -0600

    added non-canon names, modified function to allow user to specify nameset, paul helped

--- R/monNameGen.r
+++ R/monNameGen.r
@@ -1,4 +1,10 @@
-create_sample <- function(n, data, regex) {
+create_sample <- function(n, data, subst, regex) {
+  # first subset the data
+  # relying on the integer column being coerced into logical
+  data <- data[ 
+    data[["DII"]] & "DII" %in% subst |
+      data[["non-canon"]] & "non-canon" %in% subst, 1
+    ]
   sample(
     if (!is.null(regex)) 
       data[grepl(regex, data, ignore.case = TRUE)]
@@ -8,6 +14,8 @@ create_sample <- function(n, data, regex) {
     replace = TRUE
   )
 }
+
+
 
 #' Monster Name Generator
 #' 
@@ -20,6 +28,10 @@ create_sample <- function(n, data, regex) {
 #'   possibilities. The list must contain a combination of zero or more of the
 #'   following character vectors with valid regular expressions: \code{prefix},
 #'   \code{suffix}, \code{appelation}.
+#' @param nameset A character vector denoting the sets of names to pull from.
+#'  - "DII" = canon names 
+#'  - "non-canon" = names added that fit into the brutal naming theme, but are not canon DII or DIII names
+#'  
 #' @examples
 #' monNameGen(10)
 #' monNameGen(10, list(prefix= "Star", appelation = "Shade"))
@@ -29,11 +41,13 @@ create_sample <- function(n, data, regex) {
 
 monNameGen <- function(
   n = 1, 
-  filters = list()
+  filters = list(),
+  nameset = c("DII", "non-canon")
   ){
-  pre <- create_sample(n, .monNameGenData$prefix, filters$prefix)
-  suf <- create_sample(n, .monNameGenData$suffix, filters$suffix)
-  app <- create_sample(n, .monNameGenData$appel, filters$appelation)
+  
+  pre <- create_sample(n, name_tables$prefix, nameset, filters$prefix)
+  suf <- create_sample(n, name_tables$suffix, nameset, filters$suffix)
+  app <- create_sample(n, name_tables$appelation, nameset, filters$appelation)
   
   paste0(pre, suf, " the ", app)
 }
